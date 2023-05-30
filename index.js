899d278a4c354e9c7aa1c03b7003f22ba1da2d04
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-05-30 14:57:54 +0200

    Started from scratch again

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-express = require('express');
+import express from 'express';
 
-app = express();
+const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-	console.log('Example app listening on port 3000!');
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
 });
