0b710dbb2b13ad9e828974be36acffc9250a48e9
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-05-30 16:09:19 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Import packages
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import ai from "./routes/ai.js";
 
 // Middlewares
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+	origin: '*'
+  }));
+  
+app.use(bodyParser.json());
 
 // Routes
 app.use("/ai", ai);
--- routes/ai.js
+++ routes/ai.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { spawn } from 'child_process';
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
@@ -6,6 +8,30 @@ router.get("/", async (req, res, next) => {
     title: "Express Testing",
     message: "The app is working properly!",
   });
+});
+
+router.post("/", async (req, res, next) => {
+  console.log('Received a POST request!');
+  console.log(req.body);
+
+  const pythonProcess = spawn('python', ['python_file.py']);
+
+  pythonProcess.stdout.on('data', (data) => {
+    // Process the output from Python if needed
+    console.log(`Received data from Python: ${data}`);
+    res.send(data.toString()); // Send the processed data back as the response
+  });
+
+  pythonProcess.on('error', (error) => {
+    console.error(`Python process error: ${error.message}`);
+    res.status(500).send('Internal server error');
+  });
+
+  pythonProcess.on('exit', (code) => {
+    console.log(`Python process exited with code ${code}`);
+  });
+
+  res.send('Received your POST request!');
 });
 
 export default router;
